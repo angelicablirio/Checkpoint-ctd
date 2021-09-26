340de8a5f6ca735a19032eff6b5fab690f31cbda
Angelica Lirio <85036081+angelicablirio@users.noreply.github.com>
2021-09-26 16:34:47 -0300

    Checkpoint 2 - programação imperativa

---
+++ checkpoint-2-programacao-imperativa/app.js
@@ -0,0 +1,53 @@
+
+let produtos = [
+    {
+      nome: 'Smart TV',
+      valor: 4900,
+      qualidade: 10,
+      disponibilidade: true
+    },
+    {
+      nome: 'Notebook',
+      valor: 3100,
+      qualidade: 8,
+      isponibilidade: false
+    },
+    {
+      nome: 'Mesa de jantar',
+      valor: 1498,
+      qualidade: 9,
+      disponibilidade: true
+    },
+    {
+      nome: 'Luminária',
+      valor: 299,
+      qualidade: 7,
+      disponibilidade: true
+    },
+    {
+      nome: 'Poltrona',
+      valor: 999,
+      qualidade: 9,
+      disponibilidade: false
+    },
+    {
+      nome: 'Estante para livros',
+      valor: 500,
+      qualidade: 8,
+      disponibilidade: true 
+    }
+  ];
+  
+  let carrinho = produtos.filter((produto) => {
+     return produto.valor >= 482 &&
+            produto.valor <= 1600 && 
+            produto.qualidade > 6 &&
+            produto.disponibilidade === true
+  });
+  
+  let somarValorDoCarrinho = carrinho.reduce((acumulador, preco ) => acumulador.valor + preco.valor);
+  
+  let mostrarItensDoCarrinho = carrinho.map((item) => (`${item.nome} R$ ${item.valor}`));
+  
+  console.log(`Os itens do carrinho são ${mostrarItensDoCarrinho}. A soma total do carrinho é R$ ${somarValorDoCarrinho}`);
+
